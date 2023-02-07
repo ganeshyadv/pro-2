f63aa59eff5600cce1cec429526c209eccc8a465
ganeshyadv <8982270763ganesh@gmail.com>
2023-02-07 12:38:26 +0530

    create server

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+
+let port = process.env.port;
+app.listen(port, function () {
+    console.log("server started on port", port);
+})
